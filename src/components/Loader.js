ce90b9905d17f01f17dfba46d250341b0b18f955
Shubham Jha <shubham.jha2001m@gmail.com>
2021-07-29 22:40:26 +0530

    :twisted_rightwards_arrows: Loading Component

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+const Loader = () => (
+	<div className="loading">
+            <div>
+                <div class="loader"></div>
+            </div>
+
+    </div>
+);
+
+export default Loader;
