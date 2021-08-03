e75a9d14e461a2d1a9e2711f6344bed8ef59ac32
Shubham Jha <shubham.jha2001m@gmail.com>
2021-08-03 23:35:02 +0530

    `User` Registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
 
 const Register = () => {
+
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const [err, setErr] = useState('');
+
+    const history = useHistory();
+
+    const onChangeInput = e => {
+        const { name, value } = e.target;
+        setUser({...user, [name]:value});
+        setErr('');
+    }
+
+    const registerSubmit = async e =>{
+        e.preventDefault()
+        try {
+            const res = await axios.post('https://obscure-shelf-45797.herokuapp.com/users/signup',{
+                email: user.email,
+                password: user.password
+            });
+            setUser({name: '', email: '', password: ''});
+            return history.push('/login');
+        } catch (err) {
+            err.response.data.msg && setErr(err.response.data.msg)
+        }
+    }
+
     return (
         <>
             <Navbar />
@@ -10,16 +43,14 @@ const Register = () => {
                 <p>First things first. Lets create a new account and get started</p>
             </hgroup>
 
-            <form method="post" className = "log-form">
+            <form onSubmit={registerSubmit} method="post" className = "log-form">
                 <div className="group log-input">
-                    <input type="email" id = "email" name = "email" placeholder = "E-Mail" required />
+                    <input type="email" id = "email" name = "email" placeholder = "E-Mail" required value={user.email} onChange={onChangeInput} required />
                 </div>
 
                 <div className="group log-input">
-                    <input type="password" id = "password" name = "password"  placeholder = "Password" required autoComplete="true" />
+                    <input type="password" id = "password" name = "password"  placeholder = "Password" required value={user.password} onChange={onChangeInput} autoComplete="true" />
                 </div>
-
-                <br /><br />
 
                 <div className="container-log-btn">
                     <button type="submit" name = "btn_submit" className="log-form-btn">
