7c78ee11afaaad2e9f5074c582e7808e6704890f
Shubham Jha <shubham.jha2001m@gmail.com>
2021-08-03 22:44:32 +0530

    Registration `Form`

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,7 +5,28 @@ const Register = () => {
     return (
         <>
             <Navbar />
-            <h1>Register Page</h1>
+            <hgroup>
+                <h2>Welcome to <strong>Blogify</strong>!</h2>
+                <p>First things first. Lets create a new account and get started</p>
+            </hgroup>
+
+            <form method="post" className = "log-form">
+                <div className="group log-input">
+                    <input type="email" id = "email" name = "email" placeholder = "E-Mail" required />
+                </div>
+
+                <div className="group log-input">
+                    <input type="password" id = "password" name = "password"  placeholder = "Password" required autoComplete="true" />
+                </div>
+
+                <br /><br />
+
+                <div className="container-log-btn">
+                    <button type="submit" name = "btn_submit" className="log-form-btn">
+                    <span>REGISTER</span>
+                    </button>
+                </div>
+            </form>
         </>
     );
 }
